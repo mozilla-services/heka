e5ecd69b5171760da7eb2be3f99e692f7862f4a5
Ben Bangert <ben@groovie.org>
2012-11-01 11:46:26 -0700

    Fix up flags and poolsize

--- hekad/main.go
+++ hekad/main.go
@@ -4,14 +4,21 @@ import (
 	"flag"
 	"heka/pipeline"
 	"log"
+	"runtime"
 )
 
 func main() {
 	configFile := flag.String("config", "agent.conf", "Agent Config file")
+	maxprocs := flag.Int("maxprocs", 1, "Go runtime MAXPROCS value")
+	poolSize := flag.Int("poolsize", 1000, "Pipeline pool size")
+
 	flag.Parse()
+
+	runtime.GOMAXPROCS(*maxprocs)
 
 	config := new(pipeline.PipelineConfig)
 	config.Init()
+	config.PoolSize = *poolSize
 	err := pipeline.LoadFromConfigFile(*configFile, config)
 	if err != nil {
 		log.Fatal("Error reading config: ", err)
