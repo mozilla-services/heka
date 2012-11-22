235771b211ee6dd9fb4885eeed39c7f39e9a1c44
Rob Miller <rob@kalistra.com>
2012-11-22 00:10:34 -0800

    Learned about the `init()` function, no longer need the funky "define a struct and an interface and see if the struct implements the interface" kludge.

--- hekad/main.go
+++ hekad/main.go
@@ -9,23 +9,6 @@ import (
 	"runtime/pprof"
 )
 
-type PluginLoader struct{}
-
-// Interface that acts as a flag indicating whether or not the PluginLoader
-// should load plugins that have been defined in other packages.
-type LoadsExternalPlugins interface {
-	LoadExternalPlugins()
-}
-
-// Checks to see if provided object has the `LoadExternalPlugins` method and
-// calls it to load any plugins defined in external packages.
-func tryLoadExternalPlugins(maybeLoader interface{}) {
-	pluginLoader, ok := maybeLoader.(LoadsExternalPlugins)
-	if ok {
-		pluginLoader.LoadExternalPlugins()
-	}
-}
-
 func main() {
 	configFile := flag.String("config", "agent.conf", "Agent Config file")
 	maxprocs := flag.Int("maxprocs", 1, "Go runtime MAXPROCS value")
@@ -43,10 +26,6 @@ func main() {
 		pprof.StartCPUProfile(profFile)
 		defer pprof.StopCPUProfile()
 	}
-
-	// Register any external plugins w/ the `AvailablePlugins` map.
-	pluginLoader := new(PluginLoader)
-	tryLoadExternalPlugins(pluginLoader)
 
 	// Set up and load the pipeline configuration and start the daemon.
 	pipeconf := pipeline.NewPipelineConfig(*poolSize)
