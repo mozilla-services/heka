c694362d5fea51f939bbfe9be833d543c1fc93ca
Ben Bangert <ben@groovie.org>
2013-01-24 21:18:23 -0800

    Refactor to not use the plugin writer. Start the separate file monitor portion.

--- pipeline/logfile_input.go
+++ pipeline/logfile_input.go
@@ -15,51 +15,101 @@ package pipeline
 
 import (
 	"github.com/howeyc/fsnotify"
+	"sync"
 )
 
 type LogfileInputConfig struct {
 	SincedbFlush int
-	WatchFiles   []string
+	LogFiles     []string
 }
 
-type LogfileWriter struct {
+type LogfileInput struct {
+	Monitor *FileMonitor
 }
 
 type Logline struct {
-	Path []byte
+	Path string
 	Line []byte
 }
 
 func (lw *LogfileWriter) ConfigStruct() interface{} {
-	return &LogfileInputConfig{1}
+	return &LogfileInputConfig{5}
 }
 
-func (lw *LogfileWriter) MakeOutData() interface{} {
-	logline := new(Logline)
-	logline.Path = make([]byte, 500)
-	logline.Line = make([]byte, 1500)
-	return logline
-}
-
-func (lw *LogfileWriter) ZeroOutData(outData interface{}) {
-	outData.(*Logline).Path = outData.(*Logline).Path[:0]
-	outData.(*Logline).Line = outData.(*Logline).Line[:0]
-}
-
-func (lw *LogfileWriter) Init(config interface{}) error {
+func (lw *LogfileInput) Init(config interface{}) error {
+	conf := config.(*LogfileInputConfig)
+	lw.Monitor = new(FileMonitor)
+	if err := lw.Monitor.Init(conf.LogFiles); err != nil {
+		return err
+	}
 	return nil
 }
 
-// We don't actually do anything, because PrepOutData is the
-// one that copies data into the pipeline pack as an input
+func (lw *LogfileInput) Read(pipelinePack *PipelinePack,
+	timeout *time.Duration) (err error) {
+
+}
+
+func (lw *LogfileInput) Event(eventType string) {
+
+}
+
+// FileMonitor, manages a group of FileTailers
 //
-// The result of this is that the outData is then zeroed and
-// returned to the recycle pool.
-func (lw *LogfileWriter) Write(outData interface{}) error {
-	return nil
+// The FileMonitor
+type FileMonitor struct {
+	watcher   *fsnotify.Watcher
+	events    <-chan string
+	NewLines  chan *Logline
+	waitgroup *sync.WaitGroup
+	Sincedb   map[string]int64
 }
 
-func (lw *LogfileWriter) PrepOutData(pack *PipelinePack, outData interface{},
-	timeout *time.Duration) error {
+func (fm *FileMonitor) Watcher() {
+	fm.waitgroup.Add(1)
+	var event string
+	var ev *fsnotify.FileEvent
 
+	for {
+		select {
+		case event = <-fm.events:
+			if event == STOP {
+				break
+			}
+		case ev = <-fm.watcher.Event:
+			logline := make([]byte, 800)
+			line := new(Logline)
+			line.Path = ev.Name
+			line.Line = logline
+			fm.NewLines <- line
+		case fileErr := <-fm.watcher.Error:
+			nil
+		}
+	}
+	fm.waitgroup.Done()
+}
+
+func (fm *FileMonitor) Init(files []string, events <-chan string) (err error) {
+	fm.waitgroup = new(sync.WaitGroup)
+	fm.events = events
+	fm.OutLines = make(chan *Logline)
+	fm.NewLines = make(chan *Logline, 1)
+	line := new(Logline)
+	line.Path = make([]byte, 500)
+	fm.watcher, err = fsnotify.NewWatcher()
+	if err != nil {
+		return
+	}
+
+	// Launch the file reader
+	go fm.Watcher()
+
+	// Add all the files to watch
+	for fileName := range files {
+		err = fm.watcher.Watch(fileName)
+		if err != nil {
+			break
+		}
+	}
+	return
 }
