def4445b8b7a9c17d9fa5cb09c4d78b8b051d982
Ben Bangert <ben@groovie.org>
2013-01-25 15:26:58 -0800

    First possibly working attempt.

--- pipeline/logfile_input.go
+++ pipeline/logfile_input.go
@@ -14,8 +14,13 @@
 package pipeline
 
 import (
+	"bufio"
+	"errors"
 	"github.com/howeyc/fsnotify"
+	"log"
+	"os"
 	"sync"
+	"time"
 )
 
 type LogfileInputConfig struct {
@@ -25,33 +30,44 @@ type LogfileInputConfig struct {
 
 type LogfileInput struct {
 	Monitor *FileMonitor
+	logline Logline
 }
 
 type Logline struct {
 	Path string
-	Line []byte
+	Line string
 }
 
-func (lw *LogfileWriter) ConfigStruct() interface{} {
-	return &LogfileInputConfig{5}
+func (lw *LogfileInput) ConfigStruct() interface{} {
+	return &LogfileInputConfig{SincedbFlush: 1}
 }
 
-func (lw *LogfileInput) Init(config interface{}) error {
+func (lw *LogfileInput) Init(config interface{}) (err error) {
 	conf := config.(*LogfileInputConfig)
 	lw.Monitor = new(FileMonitor)
-	if err := lw.Monitor.Init(conf.LogFiles); err != nil {
+	if err = lw.Monitor.Init(conf.LogFiles); err != nil {
 		return err
 	}
 	return nil
 }
 
 func (lw *LogfileInput) Read(pipelinePack *PipelinePack,
-	timeout *time.Duration) (err error) {
-
+	timeout *time.Duration) error {
+	ticker := time.NewTicker(*timeout)
+	select {
+	case <-ticker.C:
+		return errors.New("Timeout waiting for log line")
+	case lw.logline = <-lw.Monitor.NewLines:
+		pipelinePack.Message.Type = "logfile"
+		pipelinePack.Message.Payload = lw.logline.Line
+		pipelinePack.Message.Logger = lw.logline.Path
+		pipelinePack.Decoded = true
+	}
+	return nil
 }
 
 func (lw *LogfileInput) Event(eventType string) {
-
+	lw.Monitor.Event(eventType)
 }
 
 // FileMonitor, manages a group of FileTailers
@@ -59,57 +75,140 @@ func (lw *LogfileInput) Event(eventType string) {
 // The FileMonitor
 type FileMonitor struct {
 	watcher   *fsnotify.Watcher
-	events    <-chan string
-	NewLines  chan *Logline
+	events    chan string
+	NewLines  chan Logline
 	waitgroup *sync.WaitGroup
-	Sincedb   map[string]int64
+	seek      map[string]int64
+	discover  map[string]bool
+	fds       map[string]*os.File
 }
 
 func (fm *FileMonitor) Watcher() {
-	fm.waitgroup.Add(1)
-	var event string
 	var ev *fsnotify.FileEvent
+	discovery := time.NewTicker(5)
+
+	// Attempt for files that should exist, to start reading them at
+	// the end
+	for fileName, _ := range fm.seek {
+		fm.ReadLines(fileName, nil)
+	}
 
 	for {
 		select {
-		case event = <-fm.events:
-			if event == STOP {
-				break
-			}
 		case ev = <-fm.watcher.Event:
-			logline := make([]byte, 800)
-			line := new(Logline)
-			line.Path = ev.Name
-			line.Line = logline
-			fm.NewLines <- line
-		case fileErr := <-fm.watcher.Error:
-			nil
+			fm.ReadLines(ev.Name, ev)
+		case <-fm.watcher.Error:
+			continue
+		case <-discovery.C:
+			// Check to see if the files exist now, start reading them
+			// if we can, and watch them
+			for fileName, needsWatch := range fm.discover {
+				if !needsWatch {
+					continue
+				}
+				err := fm.watcher.Watch(fileName)
+				if err != nil {
+					delete(fm.discover, fileName)
+					fm.ReadLines(fileName, nil)
+				}
+			}
 		}
 	}
-	fm.waitgroup.Done()
 }
 
-func (fm *FileMonitor) Init(files []string, events <-chan string) (err error) {
+func (fm *FileMonitor) ReadLines(fileName string, event *fsnotify.FileEvent) {
+	// First see if we have an fd for this file already
+	var seek int64
+	var fd *os.File
+	var found bool
+	if fd, found = fm.fds[fileName]; !found {
+		// Attempt to open the file
+		fd, err := os.Open(fileName)
+		if err != nil {
+			// No such file, put it back on discover
+			fm.discover[fileName] = true
+		}
+		fm.fds[fileName] = fd
+
+		// Should we seek?
+		offset := int64(0)
+		begin := os.SEEK_END
+		if seek, found = fm.seek[fileName]; found {
+			offset = seek
+			begin = 0
+		}
+		fm.seek[fileName] = offset
+		_, err = fd.Seek(offset, begin)
+		if err != nil {
+			// Unable to seek in, start at beginning
+			fd.Seek(0, 0)
+			fm.seek[fileName] = 0
+		}
+	}
+
+	// Attempt to read lines from where we are
+	reader := bufio.NewReader(fd)
+	readLine, err := reader.ReadString('\n')
+	for err == nil {
+		line := Logline{Path: fileName, Line: readLine}
+		fm.seek[fileName] += int64(len(readLine))
+		fm.NewLines <- line
+		readLine, err = reader.ReadString('\n')
+	}
+	fm.seek[fileName] += int64(len(readLine))
+
+	// Does our file still exist?
+	if event != nil {
+		if event.IsDelete() || event.IsRename() {
+			delete(fm.fds, fileName)
+			fd.Close()
+			fm.watcher.RemoveWatch(fileName)
+			fm.discover[fileName] = true
+			fm.seek[fileName] = 0
+		}
+	}
+
+	return
+}
+
+func (fm *FileMonitor) Init(files []string) (err error) {
 	fm.waitgroup = new(sync.WaitGroup)
-	fm.events = events
-	fm.OutLines = make(chan *Logline)
-	fm.NewLines = make(chan *Logline, 1)
-	line := new(Logline)
-	line.Path = make([]byte, 500)
+	fm.events = make(chan string, 1)
+	fm.NewLines = make(chan Logline, 1500)
+	fm.seek = make(map[string]int64)
+	fm.fds = make(map[string]*os.File)
+	fm.discover = make(map[string]bool)
 	fm.watcher, err = fsnotify.NewWatcher()
 	if err != nil {
 		return
 	}
 
+	// Add all the files to watch
+	for _, fileName := range files {
+		err = fm.watcher.Watch(fileName)
+		if err != nil {
+			// Error watching a file, warn and save it to discover
+			fm.discover[fileName] = true
+			log.Printf("Unable to find '%s' file, adding to discover list.",
+				fileName)
+		} else {
+			fm.seek[fileName] = 0
+		}
+	}
+
 	// Launch the file reader
 	go fm.Watcher()
 
-	// Add all the files to watch
-	for fileName := range files {
-		err = fm.watcher.Watch(fileName)
-		if err != nil {
-			break
-		}
-	}
 	return
+}
+
+// Respond to an event
+//
+// If its a STOP event, wait until the Watcher goroutine shuts down
+// gracefully
+func (fm *FileMonitor) Event(eventType string) {
+	fm.events <- eventType
+	if eventType == STOP {
+		fm.waitgroup.Wait()
+	}
 }
