45a29398d53171285c957258f142fc3a6a87de7a
rafrombrc <rob@kalistra.com>
2013-02-01 14:59:47 -0800

    Move ChainRouter out of config.go into its own file.

--- pipeline/config.go
+++ pipeline/config.go
@@ -16,8 +16,6 @@ package pipeline
 import (
 	"encoding/json"
 	"errors"
-	. "github.com/mozilla-services/heka/message"
-	"log"
 	"os"
 )
 
@@ -95,76 +93,6 @@ type ConfigFile struct {
 	Filters  []PluginConfig
 	Outputs  []PluginConfig
 	Chains   map[string]PluginConfig
-}
-
-// Represents message lookup hashes
-//
-// ChainMap is populated such that a message type should exactly
-// match and return a list representing keys in FilterChains. At the
-// moment the list will always be a single element, but in the future
-// with more ways to restrict the filter chain to other components of
-// the message narrowing down the set for several will be needed.
-type ChainRouter struct {
-	InChan   chan *PipelinePack
-	ChainMap map[string][]string
-}
-
-func (self *ChainRouter) Start() {
-	var pack *PipelinePack
-	var i int
-	go func() {
-		for {
-			pack = <-self.InChan
-			pack.OutputNames = map[string]bool{}
-			chainName, ok := self.LocateChain(pack.Message)
-			if ok {
-				pack.FilterChain = chainName
-			} else {
-				chainName = pack.FilterChain
-			}
-			chain, ok := pack.Config.FilterChains[chainName]
-			if !ok {
-				log.Printf("Filter chain doesn't exist: %s", chainName)
-				pack.Recycle()
-				continue
-			}
-			for _, outputName := range chain.Outputs {
-				pack.OutputNames[outputName] = true
-			}
-			for _, filterName := range chain.Filters {
-				filter := pack.Filters[filterName]
-				filter.FilterMsg(pack)
-				if pack.Blocked {
-					pack.Recycle()
-					continue
-				}
-			}
-
-			i = 0
-			for outputName, use := range pack.OutputNames {
-				if !use {
-					continue
-				}
-				outChan, ok := pack.OutputChans[outputName]
-				if !ok {
-					log.Printf("Output doesn't exist: %s\n", outputName)
-					continue
-				}
-				outChan <- pack
-				i++
-			}
-			if i == 0 {
-				pack.Recycle()
-			}
-		}
-	}()
-}
-
-func (self *ChainRouter) LocateChain(message *Message) (string, bool) {
-	if chains, ok := self.ChainMap[*message.Type]; ok {
-		return chains[0], true
-	}
-	return "", false
 }
 
 // A plugin wrapper wraps a plugin so that more instances of it can be
