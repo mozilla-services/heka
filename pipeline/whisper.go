2a9c71945b29caf49ed77c5c8df8a51d8df6842f
rafrombrc <rob@kalistra.com>
2013-02-26 17:05:29 -0800

    Set db intervals up correctly, further stubbing out of config setup, create folders when necessary.

--- pipeline/whisper.go
+++ pipeline/whisper.go
@@ -27,27 +27,39 @@ import (
 // WhisperRunners listen for *whisper.Point data values to come in on an input
 // channel and write the values out to the whisper db as they do.
 type WhisperRunner struct {
-	path             string
-	db               *whisper.Whisper
-	defaultAggMethod whisper.AggregationMethod
-	InChan           chan *whisper.Point
+	path   string
+	db     *whisper.Whisper
+	InChan chan *whisper.Point
 }
 
-func NewWhisperRunner(path string, aggMethod whisper.AggregationMethod) (wr *WhisperRunner,
-	err error) {
+func NewWhisperRunner(path_ string, archiveInfo []whisper.ArchiveInfo,
+	aggMethod whisper.AggregationMethod) (wr *WhisperRunner, err error) {
+
 	var db *whisper.Whisper
-	if db, err = whisper.Open(path); err != nil {
+	if db, err = whisper.Open(path_); err != nil {
 		if !os.IsNotExist(err) {
 			// A real error.
 			err = fmt.Errorf("Error opening whisper db: %s", err)
 			return
-		} else if db, err = whisper.Create(path, nil, 0.1, aggMethod, false); err != nil {
+		}
+
+		// First make sure the folder is there.
+		dir := path.Dir(path_)
+		if _, err = os.Stat(dir); os.IsNotExist(err) {
+			if err = os.MkdirAll(dir, 0700); err != nil {
+				err = fmt.Errorf("Error creating whisper db folder '%s': %s", dir, err)
+				return
+			}
+		} else if err != nil {
+			err = fmt.Errorf("Error opening whisper db folder '%s': %s", dir, err)
+		}
+		if db, err = whisper.Create(path_, archiveInfo, 0.1, aggMethod, false); err != nil {
 			err = fmt.Errorf("Error creating whisper db: %s", err)
 			return
 		}
 	}
 	inChan := make(chan *whisper.Point, 10)
-	wr = &WhisperRunner{path, db, aggMethod, inChan}
+	wr = &WhisperRunner{path_, db, inChan}
 	wr.start()
 	return
 }
@@ -67,22 +79,40 @@ func (wr *WhisperRunner) start() {
 // `statmetric` message and write the data out to a graphite-compatible
 // whisper database file tree structure.
 type WhisperOutput struct {
-	basePath         string
-	defaultAggMethod whisper.AggregationMethod
-	dbs              map[string]*WhisperRunner
+	basePath           string
+	defaultAggMethod   whisper.AggregationMethod
+	defaultArchiveInfo []whisper.ArchiveInfo
+	dbs                map[string]*WhisperRunner
 }
 
 type WhisperOutputConfig struct {
 	// Full file path to where the Whisper db files are stored.
-	BasePath         string
+	BasePath string
+
+	// Default mechanism whisper will use to aggregate data points as they
+	// roll from more precise (i.e. more recent) to less precise storage.
 	DefaultAggMethod whisper.AggregationMethod
+
+	// Slice of 3-tuples, each 3-tuple describes a time interval's storage policy:
+	// [<# of secs per datapoint> <# of datapoints> <# of secs retention>]
+	DefaultArchiveInfo [][3]uint32
 }
 
 func (o *WhisperOutput) ConfigStruct() interface{} {
 	basePath := path.Join("var", "run", "hekad", "whisper")
+
+	// 60 seconds per datapoint, 1440 datapoints = 1 day of retention
+	// 15 minutes per datapoint, 8 datapoints = 2 hours of retention
+	// 1 hour per datapoint, 7 days of retention
+	// 12 hours per datapoint, 2 years of retention
+	defaultArchiveInfo := [][3]uint32{
+		{0, 60, 1440}, {0, 900, 8}, {0, 3600, 168}, {0, 43200, 1456},
+	}
+
 	return &WhisperOutputConfig{
-		BasePath:         basePath,
-		DefaultAggMethod: whisper.AGGREGATION_AVERAGE,
+		BasePath:           basePath,
+		DefaultAggMethod:   whisper.AGGREGATION_AVERAGE,
+		DefaultArchiveInfo: defaultArchiveInfo,
 	}
 }
 
@@ -90,6 +120,10 @@ func (o *WhisperOutput) Init(config interface{}) (err error) {
 	conf := config.(*WhisperOutputConfig)
 	o.basePath = conf.BasePath
 	o.defaultAggMethod = conf.DefaultAggMethod
+	o.defaultArchiveInfo = make([]whisper.ArchiveInfo, len(conf.DefaultArchiveInfo))
+	for i, aiSpec := range conf.DefaultArchiveInfo {
+		o.defaultArchiveInfo[i] = whisper.ArchiveInfo{aiSpec[0], aiSpec[1], aiSpec[2]}
+	}
 	o.dbs = make(map[string]*WhisperRunner)
 	return
 }
@@ -108,7 +142,8 @@ func (o *WhisperOutput) Deliver(pack *PipelinePack) {
 	var unixTime uint64
 	var value float64
 	var err error
-	for _, line := range strings.Split(payload, "\n") {
+	lines := strings.Split(strings.Trim(payload, " \n"), "\n")
+	for _, line := range lines {
 		// `fields` should be "<name> <value> <timestamp>"
 		fields = strings.Fields(line)
 		if len(fields) != 3 || !strings.HasPrefix(fields[0], "stats") {
@@ -116,7 +151,8 @@ func (o *WhisperOutput) Deliver(pack *PipelinePack) {
 			continue
 		}
 		if wr = o.dbs[fields[0]]; wr == nil {
-			wr, err = NewWhisperRunner(o.getFsPath(fields[0]), o.defaultAggMethod)
+			wr, err = NewWhisperRunner(o.getFsPath(fields[0]), o.defaultArchiveInfo,
+				o.defaultAggMethod)
 			if err != nil {
 				log.Println("WhisperOutput can't create WhisperRunner: ", err)
 				continue
@@ -128,7 +164,7 @@ func (o *WhisperOutput) Deliver(pack *PipelinePack) {
 			continue
 		}
 		if value, err = strconv.ParseFloat(fields[1], 64); err != nil {
-			log.Println("WhisperOutput error parsing value: ", err)
+			log.Printf("WhisperOutput error parsing value '%s': %s", fields[1], err)
 		}
 		pt := &whisper.Point{
 			Timestamp: uint32(unixTime),
