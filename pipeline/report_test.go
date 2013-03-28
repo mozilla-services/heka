24e8c5b9e3228b9a69d3f6f1452f37aae95e8b2e
Rob Miller <rob@kalistra.com>
2013-03-28 14:39:19 -0700

    Slight test code refactor and add test for *PipelineConfig.reports() method.

--- pipeline/report_test.go
+++ pipeline/report_test.go
@@ -43,55 +43,102 @@ func ReportSpec(c gs.Context) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
+	checkForFields := func(c gs.Context, msg *message.Message) {
+		f0Val, ok := msg.GetFieldValue(f0.GetName())
+		c.Expect(ok, gs.IsTrue)
+		c.Expect(f0Val.(int64), gs.Equals, f0.GetValue().(int64))
+		f1Val, ok := msg.GetFieldValue(f1.GetName())
+		c.Expect(ok, gs.IsTrue)
+		c.Expect(f1Val.(string), gs.Equals, f1.GetValue().(string))
+	}
+
+	hasChannelData := func(msg *message.Message) (ok bool) {
+		capVal, _ := msg.GetFieldValue("InChanCapacity")
+		lenVal, _ := msg.GetFieldValue("InChanLength")
+		var i int64
+		if i, ok = capVal.(int64); !ok {
+			return
+		}
+		if ok = (i == int64(PIPECHAN_BUFSIZE)); !ok {
+			return
+		}
+		if i, ok = lenVal.(int64); !ok {
+			return
+		}
+		ok = (i == int64(0))
+		return
+	}
+
+	fName := "counter"
+	filter := new(CounterFilter)
+	fRunner := NewFORunner(fName, filter)
+
+	iName := "udp"
+	input := new(UdpInput)
+	iRunner := NewInputRunner(iName, input)
+
 	c.Specify("`PopulateReportMsg`", func() {
 		msg := getTestMessage()
 
 		c.Specify("w/ a filter", func() {
-			name := "counter"
-			filter := new(CounterFilter)
-			runner := NewFORunner(name, filter)
-			err := PopulateReportMsg(runner, msg)
+			err := PopulateReportMsg(fRunner, msg)
 			c.Assume(err, gs.IsNil)
 
 			c.Specify("invokes `ReportMsg` on the filter", func() {
-				f0Val, _ := msg.GetFieldValue(f0.GetName())
-				f1Val, _ := msg.GetFieldValue(f1.GetName())
-				c.Expect(f0Val.(int64), gs.Equals, f0.GetValue().(int64))
-				c.Expect(f1Val.(string), gs.Equals, f1.GetValue().(string))
+				checkForFields(c, msg)
 			})
 
 			c.Specify("adds the channel data", func() {
-				capVal, _ := msg.GetFieldValue("InChanCapacity")
-				lenVal, _ := msg.GetFieldValue("InChanLength")
-				c.Expect(capVal.(int64), gs.Equals, int64(PIPECHAN_BUFSIZE))
-				c.Expect(lenVal.(int64), gs.Equals, int64(0))
+				c.Expect(hasChannelData(msg), gs.IsTrue)
 			})
 		})
 
 		c.Specify("w/ an input", func() {
-			name := "udp"
-			input := new(UdpInput)
-			runner := NewInputRunner(name, input)
-			err := PopulateReportMsg(runner, msg)
+			err := PopulateReportMsg(iRunner, msg)
 			c.Assume(err, gs.IsNil)
 
 			c.Specify("invokes `ReportMsg` on the input", func() {
-				f0Val, ok := msg.GetFieldValue(f0.GetName())
-				c.Expect(ok, gs.IsTrue)
-				c.Expect(f0Val.(int64), gs.Equals, f0.GetValue().(int64))
-				f1Val, ok := msg.GetFieldValue(f1.GetName())
-				c.Expect(ok, gs.IsTrue)
-				c.Expect(f1Val.(string), gs.Equals, f1.GetValue().(string))
+				checkForFields(c, msg)
 			})
 
 			c.Specify("doesn't add any channel data", func() {
 				capVal, ok := msg.GetFieldValue("InChanCapacity")
 				c.Expect(capVal, gs.IsNil)
 				c.Expect(ok, gs.IsFalse)
-				lenVal, ok := msg.GetFieldValue("InChanCapacity")
+				lenVal, ok := msg.GetFieldValue("InChanLength")
 				c.Expect(lenVal, gs.IsNil)
 				c.Expect(ok, gs.IsFalse)
 			})
+		})
+	})
+
+	c.Specify("PipelineConfig", func() {
+		pc := NewPipelineConfig(10)
+		pc.FilterRunners = map[string]FilterRunner{fName: fRunner}
+		pc.InputRunners = map[string]InputRunner{iName: iRunner}
+
+		c.Specify("returns full set of accurate reports", func() {
+			MessageGenerator.Init()
+			reports := pc.reports()
+
+			fReport := reports[fName]
+			c.Expect(fReport, gs.Not(gs.IsNil))
+			checkForFields(c, fReport.Message)
+			c.Expect(hasChannelData(fReport.Message), gs.IsTrue)
+
+			iReport := reports[iName]
+			c.Expect(iReport, gs.Not(gs.IsNil))
+			checkForFields(c, iReport.Message)
+
+			recycleReport := reports["RecycleChan"]
+			c.Expect(recycleReport, gs.Not(gs.IsNil))
+			capVal, ok := recycleReport.Message.GetFieldValue("InChanCapacity")
+			c.Expect(ok, gs.IsTrue)
+			c.Expect(capVal.(int64), gs.Equals, int64(PoolSize+1))
+
+			routerReport := reports["Router"]
+			c.Expect(routerReport, gs.Not(gs.IsNil))
+			c.Expect(hasChannelData(routerReport.Message), gs.IsTrue)
 		})
 	})
 }
