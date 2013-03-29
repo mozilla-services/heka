b206af0d1431023ab13c1c5fd64f0c79df8ef51c
Ben Bangert <ben@groovie.org>
2013-03-28 18:04:14 -0700

    Print proper error message for failure to decode config options.

--- pipeline/config.go
+++ pipeline/config.go
@@ -259,7 +259,8 @@ func (self *PipelineConfig) loadSection(sectionName string,
 	wrapper.name = sectionName
 
 	if err = toml.PrimitiveDecode(configSection, &pluginGlobals); err != nil {
-		self.log(fmt.Sprint("Unable to locate type in plugin: %s", wrapper.name))
+		self.log(fmt.Sprintf("Unable to decode config for plugin: %s, error: %s",
+			wrapper.name, err.Error()))
 		errcnt++
 		return
 	}
