3339920a4e60ddf6139a5a7f4e8efb9aebb12b7e
Mike Trinkala <trink@acm.org>
2013-04-05 13:32:43 -0700

    Incorporate feedback from GPS's code review

--- sandbox/lua/testsupport/counter.lua
+++ sandbox/lua/testsupport/counter.lua
@@ -1,3 +1,7 @@
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 lastTime = os.time() * 1000000000
 lastCount = 0
 count = 0
