df50b278e85b446e0cfa6dcfbb3c06e20d3bb34e
Ben Bangert <ben@groovie.org>
2013-05-16 16:54:25 -0700

    Sandbox filters should be removed when exiting.

--- pipeline/pipeline_runner.go
+++ pipeline/pipeline_runner.go
@@ -287,6 +287,7 @@ func (foRunner *foRunner) Starter(h PluginHelper, wg *sync.WaitGroup) {
 
 		// If its a lua sandbox, we let it shut down
 		if _, ok := foRunner.plugin.(*SandboxFilter); ok {
+			pc.RemoveFilterRunner(foRunner.name)
 			return
 		}
 
