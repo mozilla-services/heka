7dfafaf000b34ef335016349a3f9bd28b82b6d53
Ben Bangert <ben@groovie.org>
2013-05-23 17:53:35 -0700

    Add test with a decoder.

--- pipeline/amqp_plugin_test.go
+++ pipeline/amqp_plugin_test.go
@@ -79,6 +79,7 @@ func AMQPPluginSpec(c gs.Context) {
 		encCall.Return(ith.Decoders, nil)
 
 		c.Specify("with a valid setup and no decoders", func() {
+
 			amqpInput := new(AMQPInput)
 			defaultConfig := amqpInput.ConfigStruct().(*AMQPInputConfig)
 			defaultConfig.URL = ""
@@ -161,6 +162,55 @@ func AMQPPluginSpec(c gs.Context) {
 				// Ignore leading 5 bytes of encoded message as thats the header
 				c.Expect(string(packRef.MsgBytes), gs.Equals, string(msgBody[5:]))
 				ith.PackSupply <- ith.Pack
+				close(streamChan)
+			})
+		})
+
+		c.Specify("with a valid setup using decoders", func() {
+			amqpInput := new(AMQPInput)
+			defaultConfig := amqpInput.ConfigStruct().(*AMQPInputConfig)
+			defaultConfig.URL = ""
+			defaultConfig.Exchange = ""
+			defaultConfig.ExchangeType = ""
+			defaultConfig.RoutingKey = "test"
+			defaultConfig.Decoders = []string{"defaultDecode"}
+			err := amqpInput.Init(defaultConfig)
+			c.Assume(err, gs.IsNil)
+			c.Expect(amqpInput.ch, gs.Equals, mch)
+
+			c.Specify("consumes a message", func() {
+				// Create a channel to send data to the input
+				// Drop a message on there and close the channel
+				streamChan := make(chan amqp.Delivery, 1)
+				streamChan <- amqp.Delivery{
+					ContentType: "text/plain",
+					Body:        []byte("This is a message"),
+					Timestamp:   time.Now(),
+					ConsumerTag: "TESTING",
+				}
+				mch.EXPECT().Consume("", "", false, false, false, false,
+					gomock.Any()).Return(streamChan, nil)
+
+				// Make the mock decoder runner and decoder for the 'defaultDecode'
+				dmr := NewMockDecoderRunner(ctrl)
+				mdec := NewMockDecoder(ctrl)
+				ith.MockDecoderSet.EXPECT().ByName("defaultDecode").Return(dmr, true)
+				dmr.EXPECT().Decoder().Return(mdec)
+				mdec.EXPECT().Decode(ith.Pack).Return(nil)
+
+				// Expect the injected packet
+				ith.MockInputRunner.EXPECT().Inject(gomock.Any())
+
+				// Increase the usage since Run decrements it on close
+				ug.Add(1)
+
+				ith.PackSupply <- ith.Pack
+				go func() {
+					amqpInput.Run(ith.MockInputRunner, ith.MockHelper)
+				}()
+				ith.PackSupply <- ith.Pack
+				c.Expect(ith.Pack.Message.GetType(), gs.Equals, "amqp")
+				c.Expect(ith.Pack.Message.GetPayload(), gs.Equals, "This is a message")
 				close(streamChan)
 			})
 		})
