7dfdcf836deb45e745ca36c26215684470703c5e
Divij Rajkumar <drajkuma1@gmail.com>
2013-06-30 01:13:05 -0400

    Fix for Issue #298

--- pipeline/logline_decoder.go
+++ pipeline/logline_decoder.go
@@ -135,8 +135,11 @@ func (ld *LoglineDecoder) Decode(pack *PipelinePack) (err error) {
 		if err != nil {
 			ld.dRunner.LogError(fmt.Errorf("Don't recognize Timestamp: '%s'", timeStamp))
 		}
-		// Did we get a year?
-		if val.Year() == 0 {
+		// Did we only get a timestamp?
+		if (val.Year() == 0 && val.Month() == 1 && val.Day() == 1) {
+			val = val.AddDate(time.Now().Year(), (time.Now().Month()-1), (time.Now().Day()-1))
+        // Did we get a year?
+        else if (val.Year() == 0) {
 			val = val.AddDate(time.Now().Year(), 0, 0)
 		}
 		pack.Message.SetTimestamp(val.UnixNano())
