b10895dcf731303a0757713a80df5a41b0bfebcc
Victor Ng <victor@crankycoder.com>
2013-07-10 15:39:29 -0400

    added a platform specific path separator at the beginning of the basepath

--- pipeline/whisper.go
+++ pipeline/whisper.go
@@ -129,7 +129,7 @@ type WhisperOutputConfig struct {
 }
 
 func (o *WhisperOutput) ConfigStruct() interface{} {
-	basePath := path.Join("/var", "run", "hekad", "whisper")
+	basePath := path.Join(string(os.PathSeparator), "var", "run", "hekad", "whisper")
 
 	return &WhisperOutputConfig{
 		BasePath:         basePath,
