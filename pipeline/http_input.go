1ecff479c67f959e0f8a7b327b6b67764353752f
Victor Ng <victor@crankycoder.com>
2013-07-11 10:43:12 -0400

    Added test and code to short circuit PipelinePacks into the router inchan if no decoder is defined removed a duplicate close call on the stop channel

--- pipeline/http_input.go
+++ pipeline/http_input.go
@@ -41,9 +41,10 @@ func (hi *HttpInput) Init(config interface{}) error {
 
 func (hi *HttpInput) Run(ir InputRunner, h PluginHelper) (err error) {
 	var (
-		pack    *PipelinePack
-		dRunner DecoderRunner
-		ok      bool
+		pack                *PipelinePack
+		dRunner             DecoderRunner
+		ok                  bool
+		router_shortcircuit bool
 	)
 
 	ir.LogMessage(fmt.Sprintf("[HttpInput (%s)] Running...",
@@ -51,21 +52,29 @@ func (hi *HttpInput) Run(ir InputRunner, h PluginHelper) (err error) {
 
 	go hi.Monitor.Monitor(ir)
 
-	hostname := h.PipelineConfig().hostname
+	pConfig := h.PipelineConfig()
+	hostname := pConfig.hostname
 	packSupply := ir.InChan()
 
 	dSet := h.DecoderSet()
-	if dRunner, ok = dSet.ByName(hi.decoderName); !ok {
+	if hi.decoderName == "" {
+		router_shortcircuit = true
+	} else if dRunner, ok = dSet.ByName(hi.decoderName); !ok {
 		return fmt.Errorf("Decoder not found: %s", hi.decoderName)
 	}
+
 	for {
 		select {
 		case data := <-hi.dataChan:
 			pack = <-packSupply
-			copy(pack.MsgBytes, data)
-			pack.Message.SetType("heka.httpdata")
-			pack.Message.SetHostname(hostname)
-			dRunner.InChan() <- pack
+			if router_shortcircuit {
+				pConfig.router.InChan() <- pack
+			} else {
+				copy(pack.MsgBytes, data)
+				pack.Message.SetType("heka.httpdata")
+				pack.Message.SetHostname(hostname)
+				dRunner.InChan() <- pack
+			}
 		case <-hi.stopChan:
 			return
 		}
@@ -76,7 +85,6 @@ func (hi *HttpInput) Run(ir InputRunner, h PluginHelper) (err error) {
 
 func (hi *HttpInput) Stop() {
 	close(hi.stopChan)
-	close(hi.Monitor.stopChan)
 }
 
 type HttpInputMonitor struct {
