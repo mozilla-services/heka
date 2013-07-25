83ca62e68ca5cccf44cce1fbac6e83491abfcb81
Michael Gibson <michael.gibson79@gmail.com>
2013-07-25 15:20:03 -0600

    changing regex to %\w+% per irc - RaFromBRC

--- pipeline/decoders.go
+++ pipeline/decoders.go
@@ -291,5 +291,5 @@ func InterpolateString(formatRegexp string, subs map[string]string) (newString s
 
 // Initialize the varMatcher for use in InterpolateString
 func init() {
-	varMatcher, _ = regexp.Compile("%.+?%")
+	varMatcher, _ = regexp.Compile("%\w+%")
 }
