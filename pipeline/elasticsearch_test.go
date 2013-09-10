38e5afbe1cba899aa410da28416007b724f32f6c
Michael Gibson <michael.gibson79@gmail.com>
2013-09-10 16:48:22 -0600

    adding tests in elasticsearch output for interpolatedFlag function

--- pipeline/elasticsearch_test.go
+++ pipeline/elasticsearch_test.go
@@ -60,7 +60,7 @@ func ElasticSearchOutputSpec(c gs.Context) {
 
 		c.Expect(decoded["@uuid"], gs.Equals, "87cf1ac2-e810-4ddf-a02d-a5ce44d13a85")
 		c.Expect(decoded["@timestamp"], gs.Equals, "2013-07-16T15:49:05.070Z")
-		c.Expect(decoded["@type"], gs.Equals, "TEST")
+		c.Expect(decoded["@type"], gs.Equals, "TEST-%{number}")
 		c.Expect(decoded["@logger"], gs.Equals, "GoSpec")
 		c.Expect(decoded["@severity"], gs.Equals, 6.0)
 		c.Expect(decoded["@message"], gs.Equals, "Test Payload")
@@ -69,5 +69,14 @@ func ElasticSearchOutputSpec(c gs.Context) {
 		c.Expect(decoded["@source_host"], gs.Equals, "hostname")
 
 	})
+
+        c.Specify("Should interpolate fields and message attributes for index and type names", func() {
+                interpolatedIndex := interpolateFlag(&ElasticSearchCoordinates{}, getTestMessageWithFunnyFields(), "heka-%{Pid}-%{\"foo}-%{2006.01.02}")
+                interpolatedType := interpolateFlag(&ElasticSearchCoordinates{}, getTestMessageWithFunnyFields(), "%{Type}")
+                t := time.Now()
+
+                c.Expect(interpolatedIndex, gs.Equals, "heka-14098-bar\n-" + t.Format("2006.01.02"))
+                c.Expect(interpolatedType, gs.Equals, "TEST")
+        })
 
 }
