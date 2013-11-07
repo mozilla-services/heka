9f480295189b322aee617be770c0eb2d34636523
Rob Miller <rob@kalistra.com>
2013-11-06 16:26:36 -0800

    Small fix in amqp input decoding test.

--- pipeline/amqp_plugin_test.go
+++ pipeline/amqp_plugin_test.go
@@ -140,7 +140,8 @@ func AMQPPluginSpec(c gs.Context) {
 			mockDRunner.EXPECT().Decoder().Return(mockDecoder)
 
 			c.Specify("consumes a message", func() {
-				mockDecoder.EXPECT().Decode(gomock.Any())
+				packs := []*PipelinePack{ith.Pack}
+				mockDecoder.EXPECT().Decode(ith.Pack).Return(packs, nil)
 
 				// Create a channel to send data to the input
 				// Drop a message on there and close the channel
@@ -155,6 +156,9 @@ func AMQPPluginSpec(c gs.Context) {
 				}
 				mch.EXPECT().Consume("", "", false, false, false, false,
 					gomock.Any()).Return(streamChan, nil)
+
+				// Expect the injected packet
+				ith.MockInputRunner.EXPECT().Inject(gomock.Any())
 
 				// Increase the usage since Run decrements it on close
 				ug.Add(1)
