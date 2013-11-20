83539b8a9738f1e08354bf6388ce3ff68e5068e2
Mike Trinkala <trink@acm.org>
2013-11-20 15:06:19 +0000

    Issue #525 Remove comment

--- sandbox/lua/testsupport/json_payload_transform.lua
+++ sandbox/lua/testsupport/json_payload_transform.lua
@@ -2,7 +2,7 @@
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-require "string" -- comment out if using 0.4.0
+require "string"
 require "cjson"
 
 -- sample input {"name":"android_app_created","created_at":"2013-11-15 22:37:34.709739275"}
