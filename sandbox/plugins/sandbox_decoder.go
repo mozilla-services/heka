87c95e43078e9c37509adf969e4ca8191edf2d10
Mike Trinkala <trink@acm.org>
2013-12-09 14:36:24 -0800

    Recycle all packs with the exception of the original

--- sandbox/plugins/sandbox_decoder.go
+++ sandbox/plugins/sandbox_decoder.go
@@ -207,9 +207,13 @@ func (s *SandboxDecoder) Decode(pack *pipeline.PipelinePack) (packs []*pipeline.
 	}
 	if retval < 0 {
 		atomic.AddInt64(&s.processMessageFailures, 1)
-		err = fmt.Errorf("Failed parsing: %s", s.pack.Message.GetPayload())
+		s.err = fmt.Errorf("Failed parsing: %s", s.pack.Message.GetPayload())
+		if len(s.packs) > 1 {
+			for _, p := range s.packs[1:] {
+				p.Recycle()
+			}
+		}
 		s.packs = nil
-		return
 	}
 	packs = s.packs
 	s.packs = nil
