9f8e662add69bd60f701b03dff67d06eaf6a31de
Mike Trinkala <trink@acm.org>
2014-01-16 09:21:23 -0800

    Add generalized sandbox decoder/filters
    - Update docs
    - Pull in new lua_sandbox modules
    - Increase the frequency of message processing timing samples
    - Leverage generialized plugins for FxA

--- docs/source/sandbox/frequent_item_aggregator.rst
+++
@@ -1,9 +0,0 @@
-.. _frequent_item_aggregator:
-
-Frequent Item Aggregator
-========================
-Calculates the frequent items in a data stream.  This example identifies
-the heaviest user of a system.
-
-.. literalinclude:: ../../../sandbox/lua/testsupport/sync_heavyhitters.lua
-    :language: lua 
--- sandbox/lua/filters/cbufd_aggregator.lua
+++ sandbox/lua/filters/cbufd_aggregator.lua
@@ -2,9 +2,34 @@
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+-- Collects the circular buffer delta output from multiple instances of an up
+-- stream sandbox filter (the filters should all be the same version at least
+-- with respect to their cbuf output). The purpose is to recreate the view at a
+-- larger scope in each level of the aggregation  i.e., host view ->
+-- datacenter view -> service level view.
+--
+-- Example Heka Configuration:
+--
+--  [TelemetryServerMetricsAggregator]
+--  type = "SandboxFilter"
+--  message_matcher = "Logger == 'TelemetryServerMetrics' && Fields[payload_type] == 'cbufd'"
+--  ticker_interval = 60
+--  script_type = "lua"
+--  filename = "lua_filters/cbufd_aggregator.lua"
+--  preserve_data = true
+--  memory_limit = 8000000
+--  instruction_limit = 100000
+--  output_limit = 64000
+--
+--  [TelemetryServerMetricsAggregator.config]
+--  enable_delta = false # (bool - optional default:false)
+--      # specifies whether or not this aggregator should generate cbuf deltas
+
 local cbufd = require "cbufd"
 require "circular_buffer"
 require "cjson"
+
+local enable_delta = read_config("enable_delta") or false
 
 cbufs = {}
 
@@ -14,7 +39,7 @@ function init_cbuf(payload_name, data)
         return nil
     end
 
-    local cb = circular_buffer.new(h.rows, h.columns, h.seconds_per_row)
+    local cb = circular_buffer.new(h.rows, h.columns, h.seconds_per_row, enable_delta)
     for i,v in ipairs(h.column_info) do
         cb:set_header(i, v.name, v.unit, v.aggregation)
     end
@@ -57,5 +82,8 @@ end
 function timer_event(ns)
     for k,v in pairs(cbufs) do
         inject_message(v.cbuf, k)
+        if enable_delta then
+            inject_message(v.cbuf:format("cbufd"), k)
+        end
     end
 end
