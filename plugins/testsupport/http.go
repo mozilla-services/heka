a7042ca4cea4802246ae3c8759fc0b8aff0ab392
Bart Leusink <bartleusink@gmail.com>
2014-02-21 17:53:19 +0100

    Add a test for HTTP Basic Authentication

--- plugins/testsupport/http.go
+++ plugins/testsupport/http.go
@@ -1,6 +1,7 @@
 package testsupport
 
 import (
+	"encoding/base64"
 	"fmt"
 	"net"
 	"net/http"
@@ -9,6 +10,12 @@ import (
 type OneHttpServer struct {
 	response_data string
 	listener      net.Listener
+}
+
+// HTTP server that checks for Basic Authentication credentials
+type HttpBasicAuthServer struct {
+	username, password string
+	listener           net.Listener
 }
 
 func NewOneHttpServer(response_data string, hostname string, port int) (server *OneHttpServer, e error) {
@@ -20,12 +27,34 @@ func NewOneHttpServer(response_data string, hostname string, port int) (server *
 	return server, nil
 }
 
+func NewHttpBasicAuthServer(username, password string, hostname string, port int) (server *HttpBasicAuthServer, e error) {
+	l, e := net.Listen("tcp", fmt.Sprintf("%s:%d", hostname, port))
+	if e != nil {
+		return nil, e
+	}
+	server = &HttpBasicAuthServer{username: username, password: password, listener: l}
+	return server, nil
+}
+
 func (o *OneHttpServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, o.response_data)
 	o.listener.Close()
 }
 
+func (b *HttpBasicAuthServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	auth := "Basic " + base64.StdEncoding.EncodeToString([]byte(b.username+":"+b.password))
+	if auth != r.Header.Get("Authorization") {
+		http.Error(w, "Unauthorized", 401)
+	}
+	b.listener.Close()
+}
+
 func (o *OneHttpServer) Start(urlpath string) {
 	http.Handle(urlpath, o)
 	http.Serve(o.listener, nil)
+}
+
+func (b *HttpBasicAuthServer) Start(urlpath string) {
+	http.Handle(urlpath, b)
+	http.Serve(b.listener, nil)
 }
