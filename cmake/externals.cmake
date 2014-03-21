95fb5d0ede629e67dafdcfaef17abf16087f02c8
Mike Trinkala <trink@acm.org>
2014-03-21 15:08:14 -0700

    Pull in a new lua_sandbox to fix JSON encoding of empty strings

--- cmake/externals.cmake
+++ cmake/externals.cmake
@@ -19,7 +19,7 @@ if(INCLUDE_SANDBOX)
     externalproject_add(
         ${SANDBOX_PACKAGE}
         GIT_REPOSITORY https://github.com/mozilla-services/lua_sandbox.git
-        GIT_TAG v0.6.2
+        GIT_TAG v0.6.3
         CMAKE_ARGS ${SANDBOX_ARGS}
         INSTALL_DIR ${PROJECT_PATH}
     )
