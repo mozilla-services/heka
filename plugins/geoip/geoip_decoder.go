16d65576995ffcd297c6f567d669765a13b380bb
Michael <michael.gibson79@gmail.com>
2014-04-17 00:05:41 -0600

    adding license block

--- plugins/geoip/geoip_decoder.go
+++ plugins/geoip/geoip_decoder.go
@@ -1,5 +1,20 @@
 // +build geoip
 
+/***** BEGIN LICENSE BLOCK *****
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# The Initial Developer of the Original Code is the Mozilla Foundation.
+# Portions created by the Initial Developer are Copyright (C) 2014
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#   Michael Gibson (michael.gibson79@gmail.com)
+#   Rob Miller (rmiller@mozilla.com)
+#
+# ***** END LICENSE BLOCK *****/
+
 package geoip
 
 import (
--- plugins/geoip/geoip_decoder_test.go
+++ plugins/geoip/geoip_decoder_test.go
@@ -1,3 +1,18 @@
+/***** BEGIN LICENSE BLOCK *****
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# The Initial Developer of the Original Code is the Mozilla Foundation.
+# Portions created by the Initial Developer are Copyright (C) 2014
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#   Michael Gibson (michael.gibson79@gmail.com)
+#   Rob Miller (rmiller@mozilla.com)
+#
+# ***** END LICENSE BLOCK *****/
+
 package geoip
 
 import (
