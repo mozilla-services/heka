f21060638641f802472045062ded853d7786692d
Michael Gibson <michael.x.gibson@oracle.com>
2014-04-22 10:33:34 -0600

    Removing build constraint from test and fixing more quotations around build TAGS

--- plugins/geoip/geoip_decoder_test.go
+++ plugins/geoip/geoip_decoder_test.go
@@ -1,5 +1,3 @@
-// +build geoip
-
 /***** BEGIN LICENSE BLOCK *****
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
