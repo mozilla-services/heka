0ddec4a6cad1b3afc3279e524383c14999f7ac55
Mike Trinkala <trink@acm.org>
2014-06-11 11:36:02 -0700

    Add 'sandbox' to the error message

--- sandbox/plugins/sandbox_decoder.go
+++ sandbox/plugins/sandbox_decoder.go
@@ -244,7 +244,7 @@ func (s *SandboxDecoder) Shutdown() {
 
 func (s *SandboxDecoder) Decode(pack *pipeline.PipelinePack) (packs []*pipeline.PipelinePack, err error) {
 	if s.sb == nil {
-		err = fmt.Errorf("Decoder has been terminated")
+		err = fmt.Errorf("SandboxDecoder has been terminated")
 		return
 	}
 	s.pack = pack
