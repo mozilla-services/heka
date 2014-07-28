e3273cb69cb0e8766a91383cc45f4f668a221f2d
Chance Zibolski <chance.zibolski@gmail.com>
2014-07-28 12:15:59 -0700

    Add initial diskstats decoder for file polling input

---
+++ sandbox/lua/decoders/diskstats.lua
@@ -0,0 +1,49 @@
+local l = require 'lpeg'
+l.locale(l)
+
+local space = l.space^1
+local num = l.digit^1
+
+function column(tag)
+  return space * l.Cg(num / tonumber, tag)
+end
+
+local row = column("ReadsCompleted") * column("ReadsMerged") *
+    column("SectorsRead") * column("TimeReading") * column("WritesCompleted") *
+    column("WritesMerged") * column("SectorsWritten") * column("TimeWriting") *
+    column("NumIOInProgress") * column("TimeDoingIO") *
+    column("WeightedTimeDoingIO") * l.P("\n")
+
+local grammar = l.Ct(row^1)
+
+
+local payload_keep = read_config("payload_keep")
+
+local msg = {
+    Timestamp = nil,
+    Type = "heka.stats.diskstats",
+    Payload = nil,
+    Fields = nil
+}
+
+require "cjson"
+function process_message()
+    local data = read_message("Payload")
+    local fields = grammar:match(data)
+
+    if not fields then
+        return -1
+    end
+
+    msg.Timestamp = fields.time
+    fields.time = nil
+
+    if payload_keep then
+        msg.Payload = data
+    end
+
+    msg.Fields = fields
+    inject_message(msg)
+    return 0
+end
+
