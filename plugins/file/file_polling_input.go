9454a237c4296f27cdca52f614c8caf9ebb1345d
Chance Zibolski <chance.zibolski@gmail.com>
2014-07-30 13:41:14 -0700

    Add license to file_polling_input

--- plugins/file/file_polling_input.go
+++ plugins/file/file_polling_input.go
@@ -1,3 +1,17 @@
+/***** BEGIN LICENSE BLOCK *****
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# The Initial Developer of the Original Code is the Mozilla Foundation.
+# Portions created by the Initial Developer are Copyright (C) 2014
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#   Chance Zibolski (chance.zibolski@gmail.com)
+#
+# ***** END LICENSE BLOCK *****/
+
 package file
 
 import (
