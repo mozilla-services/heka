f3b1b017690345e1c9edccb8ab1955c810553f40
Michael Trinkala <trink@acm.org>
2014-08-27 16:41:28 -0700

    Pull in the cjson update

--- cmake/externals.cmake
+++ cmake/externals.cmake
@@ -19,7 +19,7 @@ if(INCLUDE_SANDBOX)
     externalproject_add(
         ${SANDBOX_PACKAGE}
         GIT_REPOSITORY https://github.com/mozilla-services/lua_sandbox.git
-        GIT_TAG bafce0fd6145450ec3e2c3eec6f77e693640c279
+        GIT_TAG 5a127b2ad82144faa8463083fed86e67a68046c3
         CMAKE_ARGS ${SANDBOX_ARGS}
         INSTALL_DIR ${PROJECT_PATH}
     )
