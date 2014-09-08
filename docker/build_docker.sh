a39a01524ec132ee755fbd46bb9f218bc035a642
Chance Zibolski <chance.zibolski@gmail.com>
2014-09-08 14:08:52 -0700

    build_docker.sh: Exit on first failed command

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 docker build -t mozilla/heka_base ..
 docker build --rm -t mozilla/heka_build .
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -ti --name heka_build mozilla/heka_build
