89fdad8da11758bcc9dabfeb2724560d27f6cb06
Chance Zibolski <chance.zibolski@gmail.com>
2014-09-09 15:59:13 -0700

    Dont name intermediary container for build

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -2,5 +2,5 @@
 set -e
 docker build -t mozilla/heka_base ..
 docker build --rm -t mozilla/heka_build .
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -ti --name heka_build mozilla/heka_build
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -ti mozilla/heka_build
 docker rmi mozilla/heka_build
