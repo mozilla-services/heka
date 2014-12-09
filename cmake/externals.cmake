595bd10a4e01147fe4c553b4691faddfaff71bfb
Mike Trinkala <trink@acm.org>
2014-12-09 06:54:41 -0800

    Pull in the os.date sandbox fixes

--- cmake/externals.cmake
+++ cmake/externals.cmake
@@ -19,7 +19,7 @@ if(INCLUDE_SANDBOX)
     externalproject_add(
         ${SANDBOX_PACKAGE}
         GIT_REPOSITORY https://github.com/mozilla-services/lua_sandbox.git
-        GIT_TAG 238cb24d1c1a3c537d8898996640812af8f832a1
+        GIT_TAG c7e84c0a4a633126b5802859697c1355a5c75aa8
         CMAKE_ARGS ${SANDBOX_ARGS}
         INSTALL_DIR ${PROJECT_PATH}
     )
