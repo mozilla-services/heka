0ad165b35a8d815122d2f2ab4dde27adb123c6f9
Mike Trinkala <trink@acm.org>
2015-01-09 13:00:02 -0800

    Pull in the latest lua_sandbox

--- cmake/externals.cmake
+++ cmake/externals.cmake
@@ -19,7 +19,7 @@ if(INCLUDE_SANDBOX)
     externalproject_add(
         ${SANDBOX_PACKAGE}
         GIT_REPOSITORY https://github.com/mozilla-services/lua_sandbox.git
-        GIT_TAG c7e84c0a4a633126b5802859697c1355a5c75aa8
+        GIT_TAG f0fc4e82d83cf10deed0ac105506c9afd0ce4b8f
         CMAKE_ARGS ${SANDBOX_ARGS}
         INSTALL_DIR ${PROJECT_PATH}
     )
