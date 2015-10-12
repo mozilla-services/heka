2d4aa4d8c8bd25ac568e80ae270124066190ef72
Timur Batyrshin <erthad@gmail.com>
2015-10-12 13:28:24 +0300

    updated OpenTSDB encoder based on feedback from pull request

--- sandbox/lua/encoders/opentsdb_http.lua
+++ sandbox/lua/encoders/opentsdb_http.lua
@@ -31,15 +31,15 @@ Config:
 - skip_fields (string, optional, default "")
     A list of fields (separated by spaces) which will be dropped from processing.
 
-- type_as_prefix (boolean, optional, default true)
+- type_as_prefix (boolean, optional, default false)
     Prepend metrics when encoding them to OpenTSDB format by Type field.
 
-- ts_from_message (boolean, optional, default false)
+- ts_from_message (boolean, optional, default true)
     Use the Timestamp field (otherwise "now")
 
 - add_hostname_if_missing (boolean, optional, default false)
     If no 'host' tag has been seen, append one with the value of the
-    Hostname field.  Deprecated in favour of using the 'fieldfix' filter.
+    Hostname field.
 
 
 *Example Heka Configuration*
@@ -68,6 +68,8 @@ Config:
     [{"value":0.05,"timestamp":1443535912,"metric":"stats.loadavg.15MinAvg","tags":{"env":"test","host":"example.com"}},{"value":2,"timestamp":1443535912,"metric":"stats.loadavg.NumProcesses","tags":{"env":"test","host":"example.com"}},{"value":0.01,"timestamp":1443535912,"metric":"stats.loadavg.5MinAvg","tags":{"env":"test","host":"example.com"}},{"value":0,"timestamp":1443535912,"metric":"stats.loadavg.1MinAvg","tags":{"env":"test","host":"example.com"}}]
 
 --]=]
+_PRESERVATION_VERSION = read_config("preservation_version") or 0
+_PRESERVATION_VERSION = _PRESERVATION_VERSION + 1 -- force a revision update due to internal changes
 
 require "cjson"
 require "table"
@@ -79,25 +81,12 @@ l.locale(l)
 
 local flush_count     = read_config("flush_count") or 1
 local flush_delta     = read_config("flush_delta") or 0
-local tag_prefix      = read_config("tag_prefix")
+local tag_prefix      = read_config("tag_prefix") or error("`tag_prefix` setting required")
 local tag_list_str    = read_config("tag_list") or ""
 local skip_fields_str = read_config("skip_fields") or ""
 local type_as_prefix  = read_config("type_as_prefix")
 local ts_from_message = read_config("ts_from_message")
 local add_hostname    = read_config("add_hostname_if_missing")
-_PRESERVATION_VERSION = read_config("preservation_version") or 1
-
-if not tag_prefix then
-  return -1
-end
-
-if type_as_prefix == nil then
-  type_as_prefix = true
-end
-
-if add_hostname == nil then
-  add_hostname = true
-end
 
 if ts_from_message == nil then
   ts_from_message = true
@@ -113,11 +102,12 @@ for field in skip_fields_str:gmatch("[%S]+") do
   skip_fields[field] = true
 end
 
-prefix = l.P(tag_prefix)
-chars = l.alnum + l.S("_-./")
-grammar = prefix * l.C(l.alpha * chars^0)
+local prefix = l.P(tag_prefix)
+local chars = l.alnum + l.S("_-./")
+local grammar = prefix * l.C(l.alpha * chars^0)
 
 buffer = {}
+buffered_messages = 0
 last_flush = 0
 
 function flush()
@@ -125,6 +115,7 @@ function flush()
   inject_payload()
   last_flush = os.time()
   buffer = {}
+  buffered_messages = 0
 end
 
 
@@ -137,49 +128,48 @@ function process_message()
     ts = os.time()
   end
 
-  if not ts then return -1 end
-
   local typ=read_message("Type")
   local tags = {}
   local metrics = {}
 
   local msg = decode_message(read_message("raw"))
-  if not msg.Fields then return -1 end
+  if not msg.Fields then
+    return -1, "Malformed message (no fields)"
+  end
 
   for _, field in ipairs(msg.Fields) do
-    local name = field["name"]      -- these are related to way Heka works
-    local value = field["value"][1] --
+    local name = field.name      -- these are related to way Heka works
+    local value = field.value[1] --
 
-    local tag = grammar:match(name) or (tag_list[name] ~= nil and name)
+    local tag = grammar:match(name) or (tag_list[name] and name)
     if tag then
       tags[tag] = value
     else
-      if type(value) == "number" and skip_fields[name] == nil then
+      if type(value) == "number" and not skip_fields[name] then
         if type_as_prefix then
-          name = typ .. "." .. name
+          name = string.format("%s.%s", typ, name)
         end
         metrics[name] = value
       end
     end
   end
 
-  if not tags["host"] and add_hostname then
-    tags["host"] = msg.Hostname
+  if not tags.host and add_hostname then
+    tags.host = msg.Hostname
   end
-
-  opentsdb_message = {}
 
   for name, value in pairs(metrics) do
     local message = {}
-    message["metric"] = name
-    message["value"] = value
-    message["timestamp"] = ts
-    message["tags"] = tags
-    buffer[#buffer+1] = message
+    message.metric = name
+    message.value = value
+    message.timestamp = ts
+    message.tags = tags
+    buffered_messages = buffered_messages + 1
+    buffer[buffered_messages] = message
   end
 
   -- flush the buffer
-  if (flush_count > 0 and #buffer >= flush_count) or
+  if (flush_count > 0 and buffered_messages >= flush_count) or
      (flush_delta > 0 and os.time() - last_flush > flush_delta) then
     flush()
   else
